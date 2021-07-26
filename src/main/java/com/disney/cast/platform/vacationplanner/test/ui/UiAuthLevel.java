52dd3d56d594feb41c12b239cf8f47b9715a78c1
Uday-hub-code <68074369+Uday-hub-code@users.noreply.github.com>
2021-07-26 14:15:20 +0530

    Delete src directory

--- src/main/java/com/disney/cast/platform/vacationplanner/test/ui/UiAuthLevel.java
+++
@@ -1,8 +0,0 @@
-package com.disney.cast.platform.vacationplanner.test.ui;
-
-/**
- * @author walter.finkbeiner
- */
-public enum UiAuthLevel {
-    SNOWADMIN, SUPERUSER, PLANNER, LEADER, WRONG_AUTH, NOT_IN_UI_USERS_TABLE, NO_AUTH;
-}
